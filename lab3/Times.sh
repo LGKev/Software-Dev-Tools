b005bf9d1dac52456ff6dbb02a397af85cd81520
kevin kuwata <keku4547@colorado.edu>
2018-09-20 11:05:38 -0600

    I think I actually finished problem 1! I thought I had to use regex for everything but htats not true!

--- lab3/Times.sh
+++ lab3/Times.sh
@@ -8,18 +8,37 @@
 
 file="/home/kkuwata/Documents/DevTools/lab3/AthleteTimes.txt"
 
-regExMatch="([ ][0-9]{3,4})"
+touch "tmp.txt"
+touch "final.txt"
 
-while IFS=" " read -r fieldA fieldB fieldC fieldD fieldE fieldF
+while IFS=" " read -r ID First Last time1 time2 time3 
 do
-	printf 'a: %s ' "$fieldA"
-	printf 'b: %s ' "$fieldB"
-	printf 'c: %s ' "$fieldC"
-	printf 'd: %s ' "$fieldD"
-	printf 'e: %s ' "$fieldE"
-	printf 'f: %s \n' "$fieldF"
+
+	avg=0;
+	sum=0;
+
+#	printf '1: %s ' "$ID"
+#	printf '2: %s ' "$First"
+#	printf '3: %s ' "$Last"
+#	printf '4: %s ' "$time1"
+#	printf '5: %s ' "$time2"
+#	printf '6: %s ' "$time3"
+
+	sum="$((time1 + time2 + time3))"
+	avg="$((sum/3))"
+#	printf 'avg: %.2f \n'  "$avg"
+
+	echo "$ID [$avg] $Last, $First " >> "tmp.txt"
 
 
 done < "$file"
 
+print "before sort"
+cat "tmp.txt"
 
+print "========================== \n"
+
+sort -o "final.txt" "tmp.txt" -k3,3d -k4
+cat "final.txt"
+
+rm "tmp.txt"
